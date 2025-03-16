964bb60703901237e8b87e8e25090642afc0bb56
Dami Izolan <68955399+dami-i@users.noreply.github.com>
2025-03-16 14:52:09 -0300

    feat: inserir dep sqlite3 na classe SqliteDatabase

--- src/database/SqliteDatabase.ts
+++ src/database/SqliteDatabase.ts
@@ -1,17 +1,22 @@
 import path from "node:path";
+import sqlite3 from "sqlite3";
 import { Database } from "./Database";
 
 export default class SqliteDatabase implements Database {
 
 	private _path: string;
+	private _database: sqlite3.Database;
 
 	/**
 	 * @param databasePath Path to where the database file is stored. Relative to the project's root.
 	 */
 	public constructor (databasePath: string) {
 		this._path = path.resolve(databasePath);
+		this._database = new sqlite3.Database(this._path, (err) => {
+			if (err) throw new Error("Erro ao inicializar banco de dados SQLite: " + err.message);
+		});
 	}
-	
+
 	public async setup(): Promise<void> {
 		// TODO
 	}
