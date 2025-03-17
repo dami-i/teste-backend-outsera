65938435909f334db31d833458ba54e9dbf34d50
Dami Izolan <68955399+dami-i@users.noreply.github.com>
2025-03-17 00:15:56 -0300

    feat: criar campo "created_at" no banco

--- database/schema_definition.sql
+++ database/schema_definition.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS "movies" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"title"	TEXT NOT NULL,
-	"year"	INTEGER NOT NULL,
-	"studios"	TEXT NOT NULL,
-	"producers"	TEXT NOT NULL,
-	"winner"	INTEGER NOT NULL CHECK(winner in (0, 1)),
+	"id"			INTEGER 	NOT NULL UNIQUE,
+	"title"			TEXT 		NOT NULL,
+	"year"			INTEGER 	NOT NULL,
+	"studios"		TEXT 		NOT NULL,
+	"producers"		TEXT 		NOT NULL,
+	"winner"		INTEGER 	NOT NULL CHECK(winner in (0, 1)),
+	"created_at"	DATETIME 	DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY("id" AUTOINCREMENT) ON CONFLICT ROLLBACK
 );
+-- TODO: Criar compound key com title + year codificado em base64.
