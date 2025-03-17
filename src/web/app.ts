6e304c7faf7f093e606c78fc529dd5c4686f01cc
Dami Izolan <68955399+dami-i@users.noreply.github.com>
2025-03-17 01:17:10 -0300

    feat(express): inserir handlers de 404 e 500

--- src/web/app.ts
+++ src/web/app.ts
@@ -1,12 +1,19 @@
-import express from "express";
-
-import router from "./router";
+import express, { ErrorRequestHandler, RequestHandler } from "express";
 
 const app = express();
 
 app.use(express.json());
-app.use(router); // TODO prefixar com /api/v1
 
-// TODO Adicionar handlers para 404 e 500
+const notFoundHandler: RequestHandler = (_req, res) => {
+	return res.status(404).json({ error: "Not Found" });
+};
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+	console.log(err);
+	console.log("------\nOcorreu um erro que retornou 500, mas o servidor continua rodando.");
+	return res.status(500).json({ error: "Internal server error" });
+};
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export default app;
