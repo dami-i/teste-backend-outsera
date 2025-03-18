30fe9a8cd9de89788c3079d5e42437fd0757500f
Dami Izolan <68955399+dami-i@users.noreply.github.com>
2025-03-17 23:45:33 -0300

    chore/fix: mover test/ para src/test/*
    consertar build do ts

---
+++ src/test/integration.test.ts
@@ -0,0 +1,7 @@
+import { expect, describe, test } from "vitest";
+
+describe("Teste de integração", () => {
+	test("O CSV deve ser carregado no banco de dados na inicialização do servidor", () => {
+		expect;
+	});
+});
